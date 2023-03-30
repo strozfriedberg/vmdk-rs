b426c9453c27113607a0e05e2c8b480833ac5955
Vitaly Reshetyuk <vitaly.askz.maknii@gmail.com>
2023-03-30 21:15:33 +0200

    read `VmwareVmdk`

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+extern crate kaitai;
+
+use std::ops::Deref;
+use clap::Parser;
+use generated::sparse_extent_header::*;
+use generated::vmware_vmdk::*;
+
+use self::kaitai::*;
+
+pub mod generated;
+
+#[derive(Parser)]
+struct Cli {
+    /// Path to disk image
+    vmdk_path: String,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    println!("vmdk_path: '{}'", cli.vmdk_path);
+
+    let _io = BytesReader::open(cli.vmdk_path).unwrap();
+    // let res: KResult<OptRc<SparseExtentHeader>> = SparseExtentHeader::read_into(&_io, None, None);
+    // let header: OptRc<SparseExtentHeader>;
+    let res: KResult<OptRc<VmwareVmdk>> = VmwareVmdk::read_into(&_io, None, None);
+    let header: OptRc<VmwareVmdk>;
+
+    match res {
+        Ok(_) => { header = res.unwrap(); }
+        Err(e) => { panic!("{:?}", e); }
+    }
+
+    println!("header: {header:?}");
+
+    let h: VmwareVmdk = header.get().as_ref().to_owned();
+    if let Ok(grains) = &h.grain_primary() {
+        for entry in grains.deref() {
+            println!("{}", entry);
+        }
+    };
+}
