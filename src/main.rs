7d6f82059c93ff8cf5da412c5b27c8a09d44775f
Vitaly Reshetyuk <vitaly.askz.maknii@gmail.com>
2023-04-01 16:40:32 +0200

    find boot sector

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ extern crate kaitai;
 
 use std::ops::Deref;
 use clap::Parser;
-use generated::sparse_extent_header::*;
+//use generated::sparse_extent_header::*;
 use generated::vmware_vmdk::*;
 
 use self::kaitai::*;
 
 pub mod generated;
+
+mod mmapper;
+use crate::mmapper::MMapper;
 
 #[derive(Parser)]
 struct Cli {
@@ -20,7 +23,7 @@ fn main() {
 
     println!("vmdk_path: '{}'", cli.vmdk_path);
 
-    let _io = BytesReader::open(cli.vmdk_path).unwrap();
+    let _io = BytesReader::open(&cli.vmdk_path).unwrap();
     // let res: KResult<OptRc<SparseExtentHeader>> = SparseExtentHeader::read_into(&_io, None, None);
     // let header: OptRc<SparseExtentHeader>;
     let res: KResult<OptRc<VmwareVmdk>> = VmwareVmdk::read_into(&_io, None, None);
@@ -31,20 +34,18 @@ fn main() {
         Err(e) => { panic!("{:?}", e); }
     }
 
-    println!("header: {header:?}");
+//    println!("header: {header:?}");
 
-    let mut i = 0;
+    let data = MMapper::new(&cli.vmdk_path);
     let h: VmwareVmdk = header.get().as_ref().to_owned();
+    println!("h: {h:?}");
+
     if let Ok(grains) = &h.grain_secondary() {
         let entries = grains.deref().as_slice();
         let (_, entries, _) = unsafe { entries.align_to::<u32>() };
-
-        for entry in entries {
-            println!("{entry} ({entry:#04X})");
-            i += 1;
-            if i > 20 {
-                break;
-            }
-        }
+        let offs = *data.align_to::<u32>((entries[0] as usize) << 9);
+        let boot_sector_offs = (offs as usize) << 9;
+        let boot_sector = data.align_to::<[u8; 0x200]>(boot_sector_offs);
+        println!("{:02X} {:02X}", boot_sector[0x1FE], boot_sector[0x1FF]);
     };
 }
