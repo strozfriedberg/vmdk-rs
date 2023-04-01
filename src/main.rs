79a5036b967e14289f62955d813ff07343160bb1
Vitaly Reshetyuk <vitaly.askz.maknii@gmail.com>
2023-04-01 21:07:55 +0200

    load MBR and dump partitions

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,14 @@ use std::ops::Deref;
 use clap::Parser;
 //use generated::sparse_extent_header::*;
 use generated::vmware_vmdk::*;
+use generated::mbr_partition_table::*;
 
 use self::kaitai::*;
 
 pub mod generated;
+
+pub mod partition_type;
+use crate::partition_type::PartitionType;
 
 mod mmapper;
 use crate::mmapper::MMapper;
@@ -45,7 +49,23 @@ fn main() {
         let (_, entries, _) = unsafe { entries.align_to::<u32>() };
         let offs = *data.align_to::<u32>((entries[0] as usize) << 9);
         let boot_sector_offs = (offs as usize) << 9;
-        let boot_sector = data.align_to::<[u8; 0x200]>(boot_sector_offs);
-        println!("{:02X} {:02X}", boot_sector[0x1FE], boot_sector[0x1FF]);
+        let boot_sector = data.align_to::<[u8; 0x200]>(boot_sector_offs).to_vec();
+        let _io = BytesReader::from(boot_sector);
+        let partition_table: OptRc<MbrPartitionTable> = match MbrPartitionTable::read_into(&_io, None, None) {
+            Ok(pt) => pt,
+            Err(e) => panic!("{:?}", e),
+        };
+        let partition_table = partition_table.get().as_ref().to_owned();
+        for partition in &*partition_table.partitions() {
+            let partition = partition.get();
+            let partition_type: PartitionType = unsafe { std::mem::transmute(*partition.partition_type()) };
+            // let partition_type = num::FromPrimitive::from_u8(*partition.partition_type());
+            // let partition_type = if let Some(pt) = num::FromPrimitive::from_u8(*partition.partition_type()) {
+            //     format!("{:?}", pt::<PartitionType>)
+            // } else {
+            //     format!("Unknown partition type {}", partition.partition_type())
+            // };
+            println!("status: {}, partition_type {:?}, num_sectors: {}", partition.status(), partition_type, partition.num_sectors());
+        }
     };
 }
