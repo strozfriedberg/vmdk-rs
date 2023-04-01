a70d661869db595a6d0e72d1ab5ea952c25a3b30
Vitaly Reshetyuk <vitaly.askz.maknii@gmail.com>
2023-04-01 21:04:20 +0200

    `remove_inner_attrs` cleans generated `*.rs`; compile and watch `ksy/*.ksy`

--- build.rs
+++ build.rs
@@ -1,8 +1,29 @@
 use std::{
     env,
+    fs,
     str,
     process::Command
 };
+use std::io::Write;
+
+fn remove_inner_attrs(file: &str) {
+    let body = match fs::read_to_string(file) {
+        Ok(s) => s,
+        Err(e) => panic!("Could not read '{file}': {e}"),
+    };
+    let mut out_file = match fs::File::create(file) {
+        Ok(f) => f,
+        Err(e) => panic!("Could not create '{file}': {e}"),
+    };
+
+    for line in body.lines() {
+        if !line.starts_with("#![") {
+            if let Err(e) = writeln!(out_file, "{}", line) {
+                panic!("Could not write '{line}' into {file}: {e}");
+            }
+        }
+    }
+}
 
 fn main() {
     let env_var_compiler_name = "KAITAI_STRUCT_COMPILER";
@@ -24,7 +45,7 @@ fn main() {
         cmd.args(["/C", &kaitai_struct_compiler]);
     };
     let output = cmd
-            .args(["--target", "rust", "-d", out_dir.to_str().unwrap(), "ksy/sparse.ksy", "ksy/vmware_vmdk.ksy"])
+            .args(["--target", "rust", "-d", out_dir.to_str().unwrap(), "ksy/*.ksy"])
             .output()
             .expect("failed to execute process");
     let errors = output.stderr;
@@ -32,7 +53,14 @@ fn main() {
         panic!("{}", str::from_utf8(&errors).unwrap());
     }
 
+    if let Ok(entries) = fs::read_dir(out_dir) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                remove_inner_attrs(entry.path().to_str().unwrap());
+            }
+        }
+    }
+
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=ksy/sparse.ksy");
-    println!("cargo:rerun-if-changed=ksy/vmware_vmdk.ksy");
+    println!("cargo:rerun-if-changed=ksy/*.ksy");
 }
