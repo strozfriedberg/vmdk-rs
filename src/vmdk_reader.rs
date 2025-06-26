5f045fd84b1f084e64b34e9d3e2160d506935207
Joel Uckelman <joel.uckelman@aon.com>
2025-06-26 14:15:09 +0100

    Cleanup.

--- src/vmdk_reader.rs
+++ src/vmdk_reader.rs
@@ -123,11 +123,7 @@ impl VmdkReader {
 
         loop {
             let (descriptor, is_bin) = read_descriptor(&current_fn)?;
-            let extents0 = read_extents(
-                &current_fn,
-                &descriptor,
-                is_bin
-            )?;
+            let extents0 = read_extents(&current_fn, &descriptor, is_bin)?;
 
             let total_size0 = extents0.iter().fold(0u64, |acc, i| acc + i.sectors * 512);
             if total_size == 0 {
