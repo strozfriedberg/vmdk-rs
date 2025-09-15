97ab87b95390f9a87d77022e8f0f230281f95ff4
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2025-09-15 17:33:17 +0100

    Prevent rustup from clobbering itself.

--- .world/setup.sh
+++ .world/setup.sh
@@ -9,7 +9,15 @@ fi
 
 # if we're building for windows, we need to install the windows toolchain
 if [ "$Target" = 'windows' ]; then
+# rustup lock wrapper
+(
+  # Acquire the lock (blocking mode — will wait until it's free)
+  flock 9
+
+  # Critical section
   rustup target add x86_64-pc-windows-gnu
+
+) 9>/tmp/.rustup.lock
 fi
 
 # use cargo-c's lock file so that updates in its dependencies don't affect us
