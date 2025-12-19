34537935bd1ce902cb06d8f7bc1d110841ad69c4
Joel Uckelman <juckelman@strozfriedberg.co.uk>
2025-12-19 15:49:44 +0000

    Fixed reference.

--- src/capi.rs
+++ src/capi.rs
@@ -153,7 +153,7 @@ pub unsafe extern "C" fn vmdk_read(
     }
 
     let buf = unsafe { slice::from_raw_parts_mut(buf as *mut u8, buflen) };
-    unsafe { &*(*handle).reader }.read_at_offset(offset, buf)
+    unsafe { &mut *(*handle).reader }.read_at_offset(offset, buf)
         .unwrap_or_else(|e| { fill_error(e, err); 0 })
 }
 
