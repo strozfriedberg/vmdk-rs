a80c2608f044e47718306f653d281e28e2f076cc
Joel Uckelman <joel.uckelman@levelblue.com>
2026-03-23 14:43:41 +0000

    Added progress output for vmdkverify.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use bytesize::ByteSize;
 use clap::Parser;
 use sha1::{Digest, Sha1};
+use std::time::{Duration, Instant};
 use tracing_subscriber::{
     EnvFilter,
     layer::SubscriberExt,
@@ -14,11 +16,35 @@ struct Cli {
     vmdk_paths: Vec<String>,
 }
 
+fn display_progress(
+    offset: u64,
+    image_size: u64,
+    image_size_bs_disp: &bytesize::Display,
+    start: Instant
+) {
+    let offset_bs = ByteSize::b(offset);
+    eprintln!(
+        "{:.1}/{:.1} = {:.1}%, {:.1}MiB/s",
+        offset_bs.display().iec(),
+        image_size_bs_disp,
+        offset as f32 / image_size as f32 * 100.0,
+        offset_bs.as_mib() / start.elapsed().as_secs_f64()
+    );
+}
+
 fn do_hash(vmdk_path: &str) -> String /*hash*/ {
     let mut vmdk_reader = VmdkReader::open(vmdk_path).unwrap();
     let mut hasher = Sha1::new();
     let mut buf: Vec<u8> = vec![0; 1048576];
     let mut offset = 0;
+
+    let image_size_bs_disp = ByteSize::b(vmdk_reader.image_size)
+        .display()
+        .iec();
+
+    let mut prev_prog = Instant::now();
+    let start = prev_prog;
+
     while offset < vmdk_reader.image_size {
         let buf_size = buf.len();
         let read = match vmdk_reader.read_at_offset(offset, &mut buf[..buf_size]) {
@@ -35,7 +61,25 @@ fn do_hash(vmdk_path: &str) -> String /*hash*/ {
         hasher.update(&buf[..read]);
 
         offset += read as u64;
+
+        if prev_prog.elapsed() > Duration::from_secs(2) {
+            display_progress(
+                offset,
+                vmdk_reader.image_size,
+                &image_size_bs_disp,
+                start
+            );
+            prev_prog = Instant::now();
+        }
     }
+
+    display_progress(
+        offset,
+        vmdk_reader.image_size,
+        &image_size_bs_disp,
+        start
+    );
+
     let result = hasher.finalize();
     format!("{:x}", result)
 }
